f4906d0192e89e837124385a6e209cabc7be943d
Matheus Marques <marquesm91@gmail.com>
2018-06-02 01:59:41 -0300

    feat: Toggle componente com Enzyme testes

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,10 @@
 import { configure } from '@storybook/react';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 
 function loadStories() {
   require('../src/stories');
 }
 
 configure(loadStories, module);
+Enzyme.configure({ adapter: new Adapter() });
--- src/stories/Toggler.js
+++ src/stories/Toggler.js
@@ -1,10 +1,30 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import { specs, describe, it } from 'storybook-addon-specifications';
+import { shallow } from 'enzyme';
+import { expect } from 'chai';
 
 import { Toggler } from '../components';
 
 storiesOf('Toggler', module)
-  .add('with toggle', () => <Toggler onClick={action('button-click')} />);
+  .add('with toggle', () => {
+    const story = <Toggler />;
+
+    specs(() => describe('with toggle', function () {
+      it('toggle button once and have to be active', () => {
+        const wrapper = shallow(<Toggler />);
+        wrapper.find('button').simulate('click');
+        expect(wrapper.state().active).to.equal(true);
+      });
+
+      it('toggle button twice and have to be inactive', () => {
+        const wrapper = shallow(<Toggler />);
+        wrapper.find('button').simulate('click');
+        wrapper.find('button').simulate('click');
+        expect(wrapper.state().active).to.equal(true);
+      });
+    }));
+
+    return story;
+  });
